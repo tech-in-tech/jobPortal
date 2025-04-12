7703564106acb4587d5e8ee831aa206156719953
tech-in-tech <anubhavsharma06dec@gmail.com>
2025-04-12 18:34:06 +0530

    did some change in Applicants.jsx

--- Frontend/src/components/admin/Applicants.jsx
+++ Frontend/src/components/admin/Applicants.jsx
@@ -29,7 +29,7 @@ const {applicants} = useSelector(store=>store.application);
     <div>
       <Navbar/>
       <div className='max-w-7xl mx-auto'>
-        <h1 className='font-bold text-xl my-5'>Applicants {applicants.application.length}</h1>
+        <h1 className='font-bold text-xl my-5'>Applicants</h1>
         <ApplicantsTable/>
       </div>
     </div>
